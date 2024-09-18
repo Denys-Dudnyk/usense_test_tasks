f41d44373a0bcdf363e7b91501d8b30ad3604275
Denys-Dudnyk <83369962+Denys-Dudnyk@users.noreply.github.com>
2024-09-18 18:45:32 +0200

    usense test task

--- src/components/CurrencyConverter.js
+++ src/components/CurrencyConverter.js
@@ -9,8 +9,8 @@ import './CurrencyConverter.css'
 const CurrencyConverter = () => {
 	const [amount1, setAmount1] = useState(1)
 	const [amount2, setAmount2] = useState(1)
-	const [currency1, setCurrency1] = useState('USD')
-	const [currency2, setCurrency2] = useState('UAH')
+	const [currency1, setCurrency1] = useState('UAH')
+	const [currency2, setCurrency2] = useState('USD')
 
 	const { rates, loading } = useExchangeRates()
 
